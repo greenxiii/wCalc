49726049c97194e6db38ba440e788c84d3a8660b
GreenXIII <nikolayenko2009@gmail.com>
2015-01-30 00:14:54 +0200

    start project

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="styles/s.css">
+</head>
+<body>
+	<header>
+	
+	</header>
+	<section>
+		
+	</section>
+	<footer>
+		
+	</footer>
+	<script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
